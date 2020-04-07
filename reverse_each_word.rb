57b19581e912fbd57c3300bd8b2b0a923662bc7e
tiya adugna <tiyakefalee@gmail.com>
2020-04-07 22:13:47 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,9 @@
 def reverse_each_word(str)
 array = [str]
 array = str.split(/ /)
-array.collect {|str| str.reverse
-array.join(" ")}
+array.collect do |str| 
+  str.reverse
+array.join(" ")
+end
 
 end 
